7bae990c2421ae6c5196711dbd3aa7760819c0dc
Pedro Leal Lovatto <pedrolovatto99@gmail.com>
2026-03-13 09:29:09 -0300

    chore: removido imports não usados em BatalhaService

--- src/main/java/com/projeto/Batalhas/BatalhaService.java
+++ src/main/java/com/projeto/Batalhas/BatalhaService.java
@@ -1,8 +1,6 @@
 package com.projeto.Batalhas;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
